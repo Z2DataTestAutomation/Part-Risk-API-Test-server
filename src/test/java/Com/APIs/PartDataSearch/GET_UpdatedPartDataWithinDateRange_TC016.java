3578bd1e9da5db8087168417ff052ea717fdac96
Z2Data Test Automation <muhammad.sultan@z2data.com>
2020-10-19 18:54:40 +0200

    MSultan@19102020:6.45PM

--- src/test/java/Com/APIs/PartDataSearch/GET_UpdatedPartDataWithinDateRange_TC016.java
+++ src/test/java/Com/APIs/PartDataSearch/GET_UpdatedPartDataWithinDateRange_TC016.java
@@ -12,8 +12,7 @@ public class GET_UpdatedPartDataWithinDateRange_TC016 extends config {
 @Test	
 public void _UpdatedPartDataWithinDateRange () {
 	given().when().get(url + '/' + "/api/PartDataSearch/GetUpdatedPartDataWithinDateRange?Apikey=" + ViaSatAPIKey + "&fromDate=06-24-2020 01:00:00&toDate=06-24-2020 01:10:13&pagesize=10&currentpage=1").then().assertThat()
-	.body("results.Uploaded_Internal_Part_Number[1]", Is.is("Desc105")).and()
-	.body("results.Uploaded_Manufacturer_Part_Number[1]", Is.is("bb")).statusCode(200)
+	.statusCode(200)
 	.statusLine("HTTP/1.1 200 OK").header("content-Type", "application/json; charset=utf-8").log().all();
 }
 
