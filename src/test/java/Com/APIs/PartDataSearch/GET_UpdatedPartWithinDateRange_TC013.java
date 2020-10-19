078a8380b6a7431252b4dc0269005471ac5987e0
Z2Data Test Automation <muhammad.sultan@z2data.com>
2020-10-19 14:14:23 +0200

    MSultan@19102020:2.14PM

--- src/test/java/Com/APIs/PartDataSearch/GET_UpdatedPartWithinDateRange_TC013.java
+++ src/test/java/Com/APIs/PartDataSearch/GET_UpdatedPartWithinDateRange_TC013.java
@@ -12,8 +12,7 @@ public class GET_UpdatedPartWithinDateRange_TC013 extends config {
 	@Test
 	public void UpdatedPartWithinDateRange() {
 		given().when().get(url + '/' + "/api/PartDataSearch/GetUpdatedPartWithinDateRange?Apikey=" + API_Key + "&fromDate=03-01-2019&toDate=06-17-2020").then().assertThat()
-				.body("results.PartID[0]", Is.is(7795701)).and()
-				.body("results.PartNumber[0]", Is.is("ALL-2")).statusCode(200)
+				.statusCode(200)
 				.statusLine("HTTP/1.1 200 OK").header("content-Type", "application/json; charset=utf-8").log().all();
 	}
 
