757745b8b5caca242d0a284055dfb21c64562e18
zayatoo <ahmedzayat2311@gmail.com>
2021-01-14 16:56:40 +0200

    AZ1412021|04:56 PM Updating Assertions

--- src/test/java/Com/APIs/Company/GET_CompanyDirectorsByCompanyID_TC022.java
+++ src/test/java/Com/APIs/Company/GET_CompanyDirectorsByCompanyID_TC022.java
@@ -12,14 +12,13 @@ public class GET_CompanyDirectorsByCompanyID_TC022 extends config {
     public void CompanyDirectorsByCompanyID() {
 
         given().when()
-                .get(url + '/' + "/api/CompanyDataSearch/GetCompanyDirectorsByCompanyID?Apikey=" + API_Key
-                        + "&CompanyID=1006096")
+                .get(url + '/' + "/api/CompanyDataSearch/GetCompanyDirectorsByCompanyID?Apikey=" + API_Key + "&CompanyID=1006096")
                 .then().statusCode(200).statusLine("HTTP/1.1 200 OK")
-                .header("Content-Type", "application/json; charset=utf-8").and().body("results.CompanyName[0]", Is.is("Texas Instruments Incorporated"))
+                .header("Content-Type", "application/json; charset=utf-8")
+                .body("statusCode",Is.is("200"))
+                .and().body("results.CompanyName[0]", Is.is("Texas Instruments Incorporated"))
                 .log().all();
 
-
     }
-
 
 }
--- src/test/java/Com/APIs/Company/GET_CompanyRelationshipsByCompanyID_TC009.java
+++ src/test/java/Com/APIs/Company/GET_CompanyRelationshipsByCompanyID_TC009.java
@@ -9,12 +9,11 @@ public class GET_CompanyRelationshipsByCompanyID_TC009 extends config {
     @Test
     public void CompanyRelationshipsByCompanyID() {
         given().when()
-                .get(url + '/' + "api/CompanyDataSearch/GetCompanyRelationshipsByCompanyID?Apikey="
-                        + API_Key + "&CompanyID=1006096")
+                .get(url + '/' + "api/CompanyDataSearch/GetCompanyRelationshipsByCompanyID?Apikey=" + API_Key + "&CompanyID=1006096")
                 .then()
                 .statusCode(200).statusLine("HTTP/1.1 200 OK")
                 .header("Content-Type", "application/json; charset=utf-8")
-                .body("numFound", equalTo(43)).log().all();
+                .body("numFound", equalTo(44)).log().all();
     }
 
 }
